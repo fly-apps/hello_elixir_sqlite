566781014b343f2a6533a737381b5367cc9797dd
lubien <lubien1996@gmail.com>
2022-01-19 10:21:26 -0300

    Cleanup entry.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,8 @@
 
 if [ ! -f /data/prod.db ]; then
     echo "Creating database file"
-    # echo "" > /data/prod.db
     sqlite3 /data/prod.db
 fi
 
-ls -lah / && \
-    # touch /home/database.sqlite && \
-    # touch /home/database_data/database.sqlite && \
-    ls -lah /data && \
-    # /app/entry eval HelloElixir.Release.migrate && \
+/app/entry eval HelloElixir.Release.migrate && \
     /app/entry start
